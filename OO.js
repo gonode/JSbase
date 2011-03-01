ad666cb1c45df05c66da85c045dd69793fd2f4b6
gonode <gonode@gmail.com>
2011-03-01 13:53:01 -0300

    criacao inicial do jOO

--- OO.js
+++ OO.js
@@ -2,30 +2,39 @@ var OO;
 var include1, include2;
 var iFace;
 var oClass;
+var jOO = (function(undefined){
+	var _jOO = function(){
+	
+	};
+	
+	return new _jOO();
+})();
 
-OO.Class({
-	"namespace":"System",
-	"name":"Console",
-	"includes":[include1,include2,{
-		test:function(){}
-	}],
-	"implements":iFace,
-	"extends": oClass,
-	"properties":["Name"],
-	"members":["length"],
-	"methods":{
-		"Write":function(){}
-	},
-	"success":function(){},
-	"error":function(){}
-});
+(function(jOO,undefined){
+	jOO.Class({
+		"namespace":"System",
+		"name":"Console",
+		"includes":[include1,include2,{
+			test:function(){}
+		}],
+		"implements":iFace,
+		"extends": oClass,
+		"properties":["Name"],
+		"members":["length"],
+		"methods":{
+			"Write":function(){}
+		},
+		"success":function(){},
+		"error":function(){}
+	});
 
-OO.Interface({
-	"namespace":"System",
-	"name":"IConsole",
-	"interface":{
-		"Write":"method",
-		"length":"member",
-		"name":"property"
-	}
-})
+	jOO.Interface({
+		"namespace":"System",
+		"name":"IConsole",
+		"interface":{
+			"Write":"method",
+			"length":"member",
+			"name":"property"
+		}
+	});
+})(jOO);
