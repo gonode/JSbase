559de9eb56d253f3e8ab313092b9a80121e8b14f
gonode <gonode@gmail.com>
2011-03-18 08:32:12 -0300

    System.IConsole Interface

---
+++ jOO.System.IConsole.js
@@ -0,0 +1,20 @@
+(function(jOO,undefined){
+	jOO.Interface({
+		"namespace":"System",
+		"name":"IConsole",
+		"interface":{
+			"methods":["Write"],
+			"fields":["length"],
+			"properties":["name"]
+		},
+		"success":function(){
+			
+		},
+		"error":function(){
+			
+		},
+		"implement":function(){
+			
+		}
+	});
+})(jOO);
